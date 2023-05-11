644e149a045241f45b4b408db698e8643bb81c2d
Côme <127310202+ComeRegnier@users.noreply.github.com>
2023-05-11 17:18:47 +0200

    Ajout de méthode + commentaires
    Ajout de la méthode compareTo et des commentaires pour toute la classe

--- SAE/src/main/java/modele/Quete.java
+++ SAE/src/main/java/modele/Quete.java
@@ -5,18 +5,24 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Quete {
-    int numero;
-    int [] position = new int [2];
-    int [] precondition = new int [4];
-    int duree;
-    int experience;
-    String intitule;
+    private int numero;            // Numéro de la quête
+    private int [] position = new int [2];     // Coordonnées de la quête (position x,y)
+    private int [] precondition = new int [4]; // Tableau des préconditions (4 éléments max)
+    private int duree;             // Durée de la quête (en minutes)
+    private int experience;        // Points d'expérience rapportés par la quête
+    private String intitule;       // Titre de la quête
 
+    /** Constructeur de la classe Quete. Il range les données lues dans les champs correspondants.
+     *
+     * @param ligne (String) : ligne du fichier lu a traiter et ordonner
+     */
     public Quete(String ligne) {
         Scanner scanner = new Scanner(ligne).useDelimiter("\\|");
         while (scanner.hasNext()) {
+            // Lecture des différents champs de la ligne de texte pour initialiser les attributs de la quête
             numero = scanner.nextInt();
 
+            // Lecture et parsage de la position de la quête
             String pos = scanner.next();
             pos = pos.replace("(", "");
             pos = pos.replace(")", "");
@@ -28,6 +34,7 @@ public class Quete {
                 i++;
             }
 
+            // Lecture et parsage des préconditions de la quête
             String preconditions = scanner.next();
             preconditions = preconditions.replace("(", "");
             preconditions = preconditions.replace(")", "");
@@ -47,6 +54,10 @@ public class Quete {
         }
     }
 
+    /** Méthode qui retourne true si la quête n'a pas de préconditions et false sinon
+     *
+     * @return (boolean)
+     */
     public boolean sansPrecondition(){
         for(int i=0; i<4; i++){
             if(this.precondition[i]!=0){
@@ -56,7 +67,36 @@ public class Quete {
         return true;
     }
 
+
     public String toString() {
         return (numero + " " + Arrays.toString(position) + " " + Arrays.toString(precondition) + " " + duree + " " + experience + " " + intitule + "\n");
+    }
+
+
+    public int compareTo(Quete quete) {
+        if (this.numero < quete.numero) return -1;
+        return 1;
+    }
+
+    // Ci-dessous se trouvent les méthodes "get"
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public int[] getPosition() {
+        return position;
+    }
+
+    public int[] getPrecondition() {
+        return precondition;
+    }
+
+    public int getDuree() {
+        return duree;
+    }
+
+    public int getExperience() {
+        return experience;
     }
 }
