924e6351ccdd0d8dae169a4dc4c9a0d608537fad
Côme <127310202+ComeRegnier@users.noreply.github.com>
2023-05-11 17:16:46 +0200

    Ajout de commentaires

--- SAE/src/main/java/LectureScenario/LectureFichierTexte.java
+++ SAE/src/main/java/LectureScenario/LectureFichierTexte.java
@@ -8,14 +8,22 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class LectureFichierTexte {
-    public static Scenario lecture (File fichier) {
+    /** Méthode statique pour lire un fichier texte et retourner un objet Scenario
+     *
+     * @param fichier
+     * @return (scenario) : objet de la classe scénario
+     */
+    public static Scenario lecture(File fichier) {
         Scenario scenario = new Scenario();
         try {
             Scanner scanner = new Scanner(fichier);
+            // Parcourt chaque ligne du fichier texte
             while (scanner.hasNext()) {
+                // Crée un nouvel objet Quete pour chaque ligne et ajoute la quête à l'objet Scenario
                 scenario.ajout(new Quete(scanner.nextLine()));
             }
         }
+        // Capture une exception s'il y a une erreur lors de l'ouverture du fichier
         catch (FileNotFoundException e) {
             System.err.println(e.getMessage());
         }
