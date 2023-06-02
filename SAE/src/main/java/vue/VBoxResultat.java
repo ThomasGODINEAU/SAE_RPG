e665dbfe788954b5722d14539f1c8bbae2fb65fb
Côme <127310202+ComeRegnier@users.noreply.github.com>
2023-06-02 14:16:08 +0200

    Create VBoxResultat.java
    La VBox où sont affichés les meilleurs chemins demandés par l'utilisateur

---
+++ SAE/src/main/java/vue/VBoxResultat.java
@@ -0,0 +1,53 @@
+package vue;
+
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import modele.Solution;
+
+import java.util.ArrayList;
+
+public class VBoxResultat extends VBox {
+    static TableView <Solution> tableauDesSolutions;
+
+    public VBoxResultat() {
+        super(30);
+        tableauDesSolutions = new TableView<>();
+
+        TableColumn<Solution, ArrayList<Integer>> colonneCheminParcouru = new TableColumn<>("Chemin Parcouru");
+        colonneCheminParcouru.setCellValueFactory(new PropertyValueFactory<>("chemin"));
+        colonneCheminParcouru.setPrefWidth(230);
+        colonneCheminParcouru.setResizable(false);
+
+        TableColumn<Solution, Integer> colonneDuree = new TableColumn<>("Duree");
+        colonneDuree.setCellValueFactory(new PropertyValueFactory<>("duree"));
+        colonneDuree.setPrefWidth(60);
+        colonneDuree.setResizable(false);
+
+        TableColumn <Solution, Integer> colonneNombreDeQuete = new TableColumn<>("Nombre De Quetes");
+        colonneNombreDeQuete.setCellValueFactory(new PropertyValueFactory<>("nombreDeQuete"));
+        colonneNombreDeQuete.setPrefWidth(140);
+        colonneNombreDeQuete.setResizable(false);
+
+        TableColumn <Solution, Integer> colonneNombreDeCasesParcourues = new TableColumn<>("Nombre De Cases Parcourues");
+        colonneNombreDeCasesParcourues.setCellValueFactory(new PropertyValueFactory<>("nombreDeCasesParcourues"));
+        colonneNombreDeCasesParcourues.setPrefWidth(180);
+        colonneNombreDeCasesParcourues.setResizable(false);
+
+        tableauDesSolutions.getColumns().addAll(colonneCheminParcouru, colonneDuree, colonneNombreDeQuete, colonneNombreDeCasesParcourues);
+
+        getChildren().add(tableauDesSolutions);
+
+        this.update(null);
+        }
+
+        public static void update(ArrayList<Solution> listeSolutions) {
+        tableauDesSolutions.getItems().clear();
+        if(listeSolutions != null) {
+            for (Solution solution : listeSolutions) {
+                tableauDesSolutions.getItems().add(solution);
+            }
+        }
+    }
+}
