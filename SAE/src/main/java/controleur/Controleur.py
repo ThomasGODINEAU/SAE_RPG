babe3470a4c76470e05ca5c376462d16dc7f20bc
ThomasGODINEAU <129962581+ThomasGODINEAU@users.noreply.github.com>
2023-06-02 14:11:29 +0200

    Create Controleur.py
    Le controleur

---
+++ SAE/src/main/java/controleur/Controleur.py
@@ -0,0 +1,14 @@
+package controleur;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+
+public class Controleur implements EventHandler<Event> {
+    @Override
+    public void handle(Event event) {
+        if (event.getSource() instanceof Button) {
+
+        }
+    }
+}
