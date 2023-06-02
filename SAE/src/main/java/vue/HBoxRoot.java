dcddeb962327ade2cc2c0846919771b5a95113e3
Côme <127310202+ComeRegnier@users.noreply.github.com>
2023-06-02 14:25:54 +0200

    Update HBoxRoot.java
    ajout des commentaires

--- SAE/src/main/java/vue/HBoxRoot.java
+++ SAE/src/main/java/vue/HBoxRoot.java
@@ -6,13 +6,21 @@ import javafx.scene.layout.HBox;
 public class HBoxRoot extends HBox {
 
     VBoxParametres vBoxParametres = new VBoxParametres();
-    VBoxResultat vBoxResultat = new VBoxResultat();;
+    VBoxResultat vBoxResultat = new VBoxResultat();
 
+    /**
+     * Constructeur de la racine de l'interface utilisateur.
+     * Il crée et configure les conteneurs VBoxParametres et VBoxResultat,
+     * et les ajoute à la HBox principale.
+     */
     public HBoxRoot() {
+        // Configuration des marges intérieures de la HBox principale
         setPadding(new Insets(10));
 
+        // Configuration de l'ID du VBoxParametres pour les paramètres de style
         vBoxParametres.setId("parametres");
 
+        // Ajout des VBoxParametres et VBoxResultat à la HBox principale
         getChildren().addAll(vBoxParametres, vBoxResultat);
     }
 }
