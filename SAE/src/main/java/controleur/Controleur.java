91aaf28c80b3be49ce3cab5c325edd87f5077064
ThomasGODINEAU <129962581+ThomasGODINEAU@users.noreply.github.com>
2023-06-06 13:37:29 +0200

    Update Controleur.java
    Met l’événement du bouton Valider dans le contrôleur au lieu de VBoxParametres.

--- SAE/src/main/java/controleur/Controleur.java
+++ SAE/src/main/java/controleur/Controleur.java
@@ -1,14 +1,41 @@
 package controleur;
 
+import LectureScenario.LectureFichierTexte;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import modele.Niveau2;
+import modele.Personnage;
+import modele.Scenario;
+import vue.HBoxRoot;
+import vue.VBoxParametres;
+import vue.VBoxResultat;
+
+import java.io.File;
 
 public class Controleur implements EventHandler<Event> {
+
     @Override
     public void handle(Event event) {
+        VBoxParametres vBoxParametres = HBoxRoot.getvBoxParametres();
         if (event.getSource() instanceof Button) {
+                Niveau2 niveau2 = new Niveau2();
+                Scenario scenario;
+                scenario = LectureFichierTexte.lecture(new File("scenario" + File.separator + "scenario_" +
+                        vBoxParametres.getSpinnerChoixScenario().getValue() + ".txt"));
 
+                if (vBoxParametres.getGroupeBoutonsTypeChemin().getSelectedToggle().getUserData() == "Efficace") {
+                    niveau2.getCheminEfficace(new Personnage(), scenario, scenario.getListeQuetes(),
+                            vBoxParametres.getSpinnerChoixNombreCheminsVoulus().getValue(),
+                            vBoxParametres.getGroupeChoixValeurParcours().getSelectedToggle().getUserData().toString(),
+                            vBoxParametres.getGroupeBoutonsMeilleurOuPire().getSelectedToggle().getUserData().toString());
+                } else {
+                    niveau2.getCheminExhaustif(new Personnage(), scenario, scenario.getListeQuetes(),
+                            vBoxParametres.getSpinnerChoixNombreCheminsVoulus().getValue(),
+                            vBoxParametres.getGroupeChoixValeurParcours().getSelectedToggle().getUserData().toString(),
+                            vBoxParametres.getGroupeBoutonsMeilleurOuPire().getSelectedToggle().getUserData().toString());
+                }
+                VBoxResultat.update(niveau2.getListeSolutions());
+            }
         }
-    }
 }
